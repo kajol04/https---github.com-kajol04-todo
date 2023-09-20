22944596ccd24c9c3aad1822019b4941242620e9
kajol04 <kajugupta5@gmail.com>
2023-09-20 18:31:27 +0530

    https://github.com/kajol04/todo

---
+++ server/dist/js/controllers/todos/index.js
@@ -0,0 +1,91 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTodo = exports.updateTodo = exports.addTodo = exports.getTodos = void 0;
+const todo_1 = __importDefault(require("../../models/todo"));
+// GET
+// To fetch data. It receives a req and res parameter and returns a promise.
+// With the help of the Todo model created earlier, 
+// we can now get data from MongoDB and return a response with the array of todos.
+const getTodos = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todos = yield todo_1.default.find();
+        res.status(200).json({ todos });
+    }
+    catch (error) {
+        throw error;
+    }
+});
+exports.getTodos = getTodos;
+// POST
+// To receive the body object that contains data entered by the user.
+const addTodo = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const body = req.body;
+        const todo = new todo_1.default({
+            name: body.name,
+            description: body.description,
+            status: body.status,
+        });
+        const newTodo = yield todo.save();
+        const allTodos = yield todo_1.default.find();
+        res
+            .status(201)
+            .json({ message: "Todo added", todo: newTodo, todos: allTodos });
+    }
+    catch (error) {
+        throw error;
+    }
+});
+exports.addTodo = addTodo;
+// PUT
+// Using typecasting to avoid typos and restrict the body variable 
+// to match ITodo and then create a new Todo based on the model.
+// We can now save the Todo in the DB and return a response 
+// that contains the todo created and the updated todos array.
+const updateTodo = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { params: { id }, body, } = req;
+        const updateTodo = yield todo_1.default.findByIdAndUpdate({ _id: id }, body);
+        const allTodos = yield todo_1.default.find();
+        res.status(200).json({
+            message: "Todo updated",
+            todo: updateTodo,
+            todos: allTodos,
+        });
+    }
+    catch (error) {
+        throw error;
+    }
+});
+exports.updateTodo = updateTodo;
+// DELETE
+// Allows you to delete a Todo from the database. 
+// Here, we pull out the id from req and pass it as an argument to findByIdAndRemove()
+// to access the corresponding Todo and delete it from the DB.
+const deleteTodo = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const deletedTodo = yield todo_1.default.findByIdAndRemove(req.params.id);
+        const allTodos = yield todo_1.default.find();
+        res.status(200).json({
+            message: "Todo deleted",
+            todo: deletedTodo,
+            todos: allTodos,
+        });
+    }
+    catch (error) {
+        throw error;
+    }
+});
+exports.deleteTodo = deleteTodo;
